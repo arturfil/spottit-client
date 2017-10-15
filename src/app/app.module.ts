ff6298a9c7ba3a589950a0e3b765c000645c5d75
Arturo Filio Villa <arturfil@hotmail.com>
2017-10-15 17:56:06 -0500

    app module setup and planning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FileUploadModule } from 'ng2-file-upload';
+
+//services
+
+//components
 
 import { AppComponent } from './app.component';
 
