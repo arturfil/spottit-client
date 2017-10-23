d35fd75576bd07bb3c1eaeeae798fcb6a295a2c4
Arturo Filio Villa <arturfil@hotmail.com>
2017-10-23 13:37:31 -0500

    added spot-router constant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,15 @@ import { IndexComponent } from './pages/index/index.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SpotsListComponent } from './pages/spots-list/spots-list.component';
+import { MySpotsComponent } from './pages/my-spots/my-spots.component';
 
 
 const routes: Routes = [
   {path: '', component: IndexComponent },
   {path: 'signup', component: SignUpComponent },
   {path: 'login', component: LoginComponent },
-  {path: 'spots', component: SpotsListComponent }
+  {path: 'spots', component: SpotsListComponent },
+  {path: 'myspots', component: MySpotsComponent }
 ]
 
 @NgModule({
