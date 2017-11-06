c4962177a9552b89870dca4982b4c27b6f50c327
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-06 00:02:07 -0500

    changed the spots routes

--- src/app/components/spot-form/spot-form.component.ts
+++ src/app/components/spot-form/spot-form.component.ts
@@ -14,13 +14,6 @@ import { environment } from '../../../environments/environment';
 })
 export class SpotFormComponent implements OnInit {
 
-  myUploader = new FileUploader(
-    {
-      url: environment.apiUrl + '/api/spots',
-      itemAlias: 'spotImage'
-    }
-  )
-
   newSpot: SpotInfo = {
     spotName: '',
     spotAddress: '',
@@ -44,32 +37,32 @@ export class SpotFormComponent implements OnInit {
     // send 'this.newSpot' for the backend for saving
     this.spotThang.postSpot(this.newSpot)
       .subscribe(
-        // SUCCESS (1st argument of subscribe())
-        (fullSpotDetails) => {
-          console.log('New spot success', fullSpotDetails);
+      // SUCCESS (1st argument of subscribe())
+      (fullSpotDetails) => {
+        console.log('New spot success', fullSpotDetails);
 
-          // notify the parent about the new spot through the output
-          this.newSpotNotifier.emit(fullSpotDetails);
+        // notify the parent about the new spot through the output
+        this.newSpotNotifier.emit(fullSpotDetails);
 
-          this.errorMessage = '';
-          this.newSpot = {
-            spotName: '',
-            spotAddress: '',
-            spotImage: '',
-            spotWorkout: ''
-          };
-        },
+        this.errorMessage = '';
+        this.newSpot = {
+          spotName: '',
+          spotAddress: '',
+          spotImage: '',
+          spotWorkout: ''
+        };
+      },
 
-        // ERROR!(2nd argument of subscribe())
-        (errorInfo) => {
-          console.log('New phone error', errorInfo);
+      // ERROR!(2nd argument of subscribe())
+      (errorInfo) => {
+        console.log('New phone error', errorInfo);
 
-          if (errorInfo.status === 400) {
-            this.errorMessage = 'Validation error.';
-          } else {
-            this.errorMessage = 'Unknown error. Try again later.'
-          }
+        if (errorInfo.status === 400) {
+          this.errorMessage = 'Validation error.';
+        } else {
+          this.errorMessage = 'Unknown error. Try again later.'
         }
+      }
       ); // .subscribe()
   } // saveNewspot()
 }
