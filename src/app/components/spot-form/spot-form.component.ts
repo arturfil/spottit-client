18b90341e6cd16f2585b069297bed708106e8bd2
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-05 22:55:49 -0500

    changed the form-component file to get rid of multer

--- src/app/components/spot-form/spot-form.component.ts
+++ src/app/components/spot-form/spot-form.component.ts
@@ -41,59 +41,15 @@ export class SpotFormComponent implements OnInit {
   }
 
   saveNewSpot() {
-    if (this.myUploader.getNotUploadedItems().length > 0) {
-      this.saveSpotWithImage()
-    } else {
-      this.saveSpotWithNoImage()
-    }
-  }
-
-  saveSpotWithImage() {
-    this.myUploader.onBuildItemForm = (spot, form) => {
-      form.append('spotName', this.newSpot.spotName);
-      form.append('spotAddress', this.newSpot.spotAddress);
-      form.append('spotWorkout', this.newSpot.spotWorkout);
-    }
-
-    this.myUploader.onSuccessItem = (spot, response) => {
-      const fullSpotDetails = JSON.parse(response);
-      console.log('New spot success', fullSpotDetails);
-
-      // notify the parent about the new item through the output
-      this.newSpotNotifier.emit({
-        spot: fullSpotDetails,
-        queryInput: this.queryInput
-      });
-
-      this.errorMessage = '';
-      this.newSpot = {
-        spotName: '',
-        spotAddress: '',
-        spotImage: '',
-        spotWorkout: ''
-      }
-    }
-
-    this.myUploader.onErrorItem = (item, response) => {
-      console.log("New spot error", response);
-
-      this.errorMessage = "Unknown error, try again later"
-    } // on Error Item
-
-    //start ajax request
-    this.myUploader.uploadAll();
-  }
-
-  saveSpotWithNoImage() {
-    //send 'this.newSpot' to the backend for saving
+    // send 'this.newSpot' for the backend for saving
     this.spotThang.postSpot(this.newSpot)
       .subscribe(
+        // SUCCESS (1st argument of subscribe())
         (fullSpotDetails) => {
-          console.log('New spot succes', fullSpotDetails);
-          this.newSpotNotifier.emit({
-            item: fullSpotDetails,
-            queryInput: this.queryInput
-          });
+          console.log('New spot success', fullSpotDetails);
+
+          // notify the parent about the new spot through the output
+          this.newSpotNotifier.emit(fullSpotDetails);
 
           this.errorMessage = '';
           this.newSpot = {
@@ -101,17 +57,19 @@ export class SpotFormComponent implements OnInit {
             spotAddress: '',
             spotImage: '',
             spotWorkout: ''
-          }
+          };
         },
+
+        // ERROR!(2nd argument of subscribe())
         (errorInfo) => {
-          console.log('New item error', errorInfo);
-          if(errorInfo.status === 400) {
-            this.errorMessage = 'Validation Error.'
+          console.log('New phone error', errorInfo);
+
+          if (errorInfo.status === 400) {
+            this.errorMessage = 'Validation error.';
           } else {
             this.errorMessage = 'Unknown error. Try again later.'
           }
         }
-      )
-  }
-
+      ); // .subscribe()
+  } // saveNewspot()
 }
